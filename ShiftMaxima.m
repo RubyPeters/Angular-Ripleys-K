711beee384ce839cbe7b86f39e8ecda02d079578
Ed Rogers <etr@orc.soton.ac.uk>
2017-02-13 06:09:44 -0600

    Vectorise ShiftMaxima

--- ShiftMaxima.m
+++ ShiftMaxima.m
@@ -1,17 +1,5 @@
 function [ShiftedKValues] = ShiftMaxima(KValues)
     
-    S = size(KValues,1);
-    [MaxValue, MaxIndex] = max(KValues(:));
-    ShiftedKValues = zeros(S,1);
-    
-    for i=1:S
-      
-        if (MaxIndex-1+i) <= S
-            ShiftedKValues(i) = KValues(MaxIndex-1+i);
-        else
-            ShiftedKValues(i) = KValues(MaxIndex-S+i-1);
-        end
-        
-    end
-    
+    [~, MaxIndex] = max(KValues(:));
+    ShiftedKValues = circshift(KValues, -(MaxIndex-1));
 end
